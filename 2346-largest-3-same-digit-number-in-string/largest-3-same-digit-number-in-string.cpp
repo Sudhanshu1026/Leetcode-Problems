fe653aaa6ad2736c103ebb810178aa512a9bd000
Sudhanshu Singh <sudhanshusingh1026@gmail.com>
2025-08-14 14:14:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (52.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        // Assign 'maxDigit' to the NUL character (smallest ASCII value character)
-        char maxDigit = '\0';
-
-        // Iterate on characters of the num string.
-        for (int index = 0; index <= num.size() - 3; ++index) {
-            // If 3 consecutive characters are the same,
-            // store the character in 'maxDigit' if bigger than what it already stores.
-            if (num[index] == num[index + 1] && num[index] == num[index + 2]) {
-                maxDigit = max(maxDigit, num[index]);
+        string best = "";
+        for (int i = 0; i + 2 < num.size(); i++) {
+            if (num[i] == num[i+1] && num[i] == num[i+2]) {
+                best = max(best, num.substr(i, 3));
             }
         }
-
-        // If 'maxDigit' is NULL return an empty string, otherwise string of size 3 with 'maxDigit' characters.
-        return maxDigit == '\0' ? "" : string(3, maxDigit);
+        return best;
     }
 };
