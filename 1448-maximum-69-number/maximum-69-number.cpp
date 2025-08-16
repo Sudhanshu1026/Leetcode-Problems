fdc0fcb8dcf7d1a8ad358f7ea19e60d9de567d11
Sudhanshu Singh <sudhanshusingh1026@gmail.com>
2025-08-16 22:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (41.82%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        vector<int>digits;
+
+         while (num > 0) {
+        digits.push_back(num % 10);          // extract last digit
+        num /= 10;                           // remove last digit
+    }
+
+    reverse(digits.begin(), digits.end()); // reverse to correct order
+
+         for(int i=0;i<digits.size();i++)
+         {
+            if(digits[i]== 6 ){
+                digits[i]= 9 ;
+                break;
+            }
+           
+         }
+        
+         // rebuild the number
+        int result = 0;
+        for (int d : digits) {
+            result = result * 10 + d;
+        }
+
+        return result;
+
+    }
+};
