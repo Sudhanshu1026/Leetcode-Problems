fcb96f9d48ee2f9171d91586174cb10f9e6e51bf
Sudhanshu Singh <sudhanshusingh1026@gmail.com>
2025-09-04 02:50:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (60.93%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
