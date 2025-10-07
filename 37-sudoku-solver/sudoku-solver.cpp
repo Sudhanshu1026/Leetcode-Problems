abf41482b75e81f55c563677a4ff1377ff3e1cc6
Sudhanshu Singh <sudhanshusingh1026@gmail.com>
2025-10-08 00:52:26 +0530

    Time: 223 ms (72.58%) | Memory: 8.9 MB (31.06%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,37 +1,59 @@
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-    }
-    
-private:
-    bool solve(vector<vector<char>>& board) {
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] == '.') {
-                    for (char d = '1'; d <= '9'; d++) {
-                        if (isValid(board, r, c, d)) {
-                            board[r][c] = d;
-                            if (solve(board)) return true; // recursion
-                            board[r][c] = '.'; // backtrack
-                        }
-                    }
-                    return false; // no valid digit, backtrack
-                }
-            }
-        }
-        return true; // solved
+
+bool check(int num, vector<vector<char>>& board, int i, int j) {
+    char c = '0' + num;
+
+    // Check For same Row
+    for (int col = 0; col < 9; col++) {
+        if (board[i][col] == c)
+            return false;
     }
 
-    bool isValid(vector<vector<char>>& board, int r, int c, char d) {
-        for (int i = 0; i < 9; i++) {
-            // check row and col
-            if (board[r][i] == d || board[i][c] == d) return false;
-            // check 3x3 box
-            int boxRow = 3 * (r / 3) + i / 3;
-            int boxCol = 3 * (c / 3) + i % 3;
-            if (board[boxRow][boxCol] == d) return false;
-        }
-        return true;
+    // Check For same Column
+    for (int row = 0; row < 9; row++) {
+        if (board[row][j] == c)
+            return false;
     }
+
+    // Check For same Box (3x3)
+    int row = (i / 3) * 3, col = (j / 3) * 3;
+    for (int a = row; a < row + 3; a++) {
+        for (int b = col; b < col + 3; b++) {
+            if (board[a][b] == c)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool find(int i, int j, vector<vector<char>>& board) {
+    // Base Condition
+    if (i == 9)
+        return true;
+
+    if (j == 9)
+        return find(i + 1, 0, board);
+
+    if (board[i][j] != '.')
+        return find(i, j + 1, board);
+
+   
+    for (int num = 1; num <= 9; num++) {
+        if (check(num, board, i, j)) {
+            board[i][j] = '0' + num;
+
+            if (find(i, j + 1, board))
+                return true;
+            board[i][j] = '.';
+        }
+    }
+
+    return false;
+}
+
+void solveSudoku(vector<vector<char>>& board) {
+    find(0, 0, board);
+}
 };
